3688b48b558303e5350a30386ca6f7f8aee0b84b
sirSayed98 <mohamed.sayed8899@gmail.com>
2020-07-03 23:22:40 +0200

    create user

--- controllers/user.js
+++ controllers/user.js
@@ -5,11 +5,11 @@ const User = require('../models/User');
 const filterObj = (obj, ...allowedFields) => {
     const newObj = {};
     Object.keys(obj).forEach(el => {
-      if (allowedFields.includes(el)) newObj[el] = obj[el];
+        if (allowedFields.includes(el)) newObj[el] = obj[el];
     });
     return newObj;
-  };
-  
+};
+
 
 //@desc       Update date
 //@route      Put/api/v1/users
@@ -45,16 +45,28 @@ exports.updateMe = asyncHandler(async (req, res, next) => {
 // @access    Private/Admin
 exports.getUsers = asyncHandler(async (req, res, next) => {
     res.status(200).json(res.advancedResults);
-  });
-  
+});
+
+// @desc      Create user
+// @route     POST /api/v1/auth/users
+// @access    Private/Admin
+exports.createUser = asyncHandler(async (req, res, next) => {
+    const user = await User.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        data: user
+    });
+});
+
 // @desc      Delete user
 // @route     DELETE /api/v1/auth/users/:id
 // @access    Private/Admin
 exports.deleteUser = asyncHandler(async (req, res, next) => {
     await User.findByIdAndDelete(req.params.id);
-  
+
     res.status(200).json({
-      success: true,
-      data: {}
+        success: true,
+        data: {}
     });
 });
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,8 @@ const express = require('express');
 const {
     updateMe,
     deleteUser,
-    getUsers
+    getUsers,
+    createUser
 } = require('../controllers/user');
 
 const User = require('../models/User');
@@ -15,9 +16,14 @@ const router = express.Router();
 router
     .route('/')
     .put(protect, updateMe)
+
 router
     .route('/')
     .get(protect, getUsers)
+
+router
+    .route('/')
+    .post(protect, createUser)
 router
     .route('/')
     .delete(protect, deleteUser)
