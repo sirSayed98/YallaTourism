2a10965d9aa12ab4e5a52ff755045ab37fcfb7f3
sirSayed98 <mohamed.sayed8899@gmail.com>
2020-07-05 14:49:20 +0200

    add Review model

---
+++ models/Review.js
@@ -0,0 +1,35 @@
+const reviewSchema = new mongoose.Schema(
+    {
+        review: {
+            type: String,
+            required: [true, 'Review can not be empty!']
+        },
+        rating: {
+            type: Number,
+            min: 1,
+            max: 5
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        tour: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Tour',
+            required: [true, 'Review must belong to a tour.']
+        },
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+            required: [true, 'Review must belong to a user']
+        }
+    },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true }
+    }
+);
+
+module.exports = mongoose.model('Review', reviewSchema);
+
+ 
