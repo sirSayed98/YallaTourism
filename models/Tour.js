fc6197149cef4c0ffe6b562de6538f176b491566
sirSayed98 <mohamed.sayed8899@gmail.com>
2020-07-05 12:56:53 +0200

    GeoSpatial Data

--- models/Tour.js
+++ models/Tour.js
@@ -77,7 +77,31 @@ const tourSchema = new mongoose.Schema(
     secretTour: {
       type: Boolean,
       default: false
-    }
+    },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ]
   },
   {
     toJSON: { virtuals: true },
