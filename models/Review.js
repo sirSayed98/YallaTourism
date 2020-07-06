f40f69520afff55bd1433c78bbc16303d05701bc
sirSayed98 <mohamed.sayed8899@gmail.com>
2020-07-06 12:20:41 +0200

    add average reviews

--- models/Review.js
+++ models/Review.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tours = require('./Tour')
 const reviewSchema = new mongoose.Schema(
     {
         review: {
@@ -30,7 +31,52 @@ const reviewSchema = new mongoose.Schema(
         toObject: { virtuals: true }
     }
 );
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourId }
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating' }
+            }
+        }
+    ]);
+    if (stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: stats[0].nRating,
+            ratingsAverage: stats[0].avgRating
+        });
+    } else {
+        //set Default
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: 0,
+            ratingsAverage: 4.5
+        });
+    }
+};
+
+// Create Review
+reviewSchema.post('save', function () {
+    // this points to current review
+    this.constructor.calcAverageRatings(this.tour);
+    // no next() in post middleware
+});
+
+//Update and delete
+
+// pass tourId from pre middleware to post middleware --> (keep current document in this.r)  
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+    this.r = await this.findOne();
+    next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+    await this.r.constructor.calcAverageRatings(this.r.tour);
+});
+
 
 module.exports = mongoose.model('Review', reviewSchema);
 
- 
