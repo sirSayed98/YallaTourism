4fcee37514a8410fac29a15d57c0cb389ab3b174
sirSayed98 <mohamed.sayed8899@gmail.com>
2020-07-09 10:49:59 +0200

    add mapbox in tour

--- Public/js/mapbox.js
+++ Public/js/mapbox.js
@@ -1,15 +1,13 @@
 /* eslint-disable */
-export const displayMap = locations => {
-  mapboxgl.accessToken =
-    'pk.eyJ1Ijoiam9uYXNzY2htZWR0bWFubiIsImEiOiJjam54ZmM5N3gwNjAzM3dtZDNxYTVlMnd2In0.ytpI7V7w7cyT1Kq5rT9Z1A';
-
+const locations=JSON.parse(document.getElementById('map').dataset.locations);
+  mapboxgl.accessToken = 'pk.eyJ1Ijoic2llc2F5ZWQ5OCIsImEiOiJja2NlaTlzN3cwOGd5MnNtMmp2ZTd6eHRiIn0.1PemqqkDrkKvJyuCQ_N9Lw';
   var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/jonasschmedtmann/cjvi9q8jd04mi1cpgmg7ev3dy',
-    scrollZoom: false
-    // center: [-118.113491, 34.111745],
-    // zoom: 10,
-    // interactive: false
+    style: 'mapbox://styles/siesayed98/ckceiw8fq0psw1imgzp9j3zhf',
+    // scrollZoom: false,
+    //center: [-118.113491, 34.111745],
+     zoom: 3,
+    // interactive: false 
   });
 
   const bounds = new mapboxgl.LngLatBounds();
@@ -47,4 +45,4 @@ export const displayMap = locations => {
       right: 100
     }
   });
-};
+
