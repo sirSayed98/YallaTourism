4e258f07b5316e3efdd39634044cb9e2e877932c
sirSayed98 <mohamed.sayed8899@gmail.com>
2020-07-12 18:52:37 +0200

    create booking

--- controllers/booking.js
+++ controllers/booking.js
@@ -1,6 +1,8 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Tour = require('../models/Tour');
+const Booking = require('../models/Booking');
+
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 // @desc      BOOK TOUR
@@ -18,7 +20,9 @@ exports.getCheckoutSession = asyncHandler(async (req, res, next) => {
     // 2) Create checkout session
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
-        success_url: `${req.protocol}://${req.get('host')}/`,
+        success_url: `${req.protocol}://${req.get('host')}/?tour=${
+            req.params.tourID
+            }&user=${req.user.id}&price=${tour.price}`,
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourID,
@@ -41,6 +45,19 @@ exports.getCheckoutSession = asyncHandler(async (req, res, next) => {
         status: 'success',
         data: session
     });
+});
+
+// @desc      Book Tour
+// @route     Post /api/v1/booking/
+// @access    Private
+exports.createBookingCheckout = asyncHandler(async (req, res, next) => {
+    //this only for developing ... change it later
+    const { tour, user, price } = req.query;
+    if (!tour || !user || !price)
+        return next();
+    await Booking.create({ tour, user, price });
+
+    res.redirect(req.originalUrl.split('?')[0])
 });
 
 
--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -7,12 +7,14 @@ const {
     getLoginForm,
     getProfile,
 } = require('../controllers/viewsController');
+const { createBookingCheckout,getMyTours } = require('../controllers/booking');
 
 const router = express.Router();
-const { isLoggedIn,protectFront } = require('../middleware/auth');
+const { isLoggedIn, protectFront } = require('../middleware/auth');
 
-router.get('/',isLoggedIn,getOverview);
-router.get('/tour/:slug',isLoggedIn,getTour);
-router.get('/login', isLoggedIn,getLoginForm);
-router.get('/profile', protectFront,getProfile);
+router.get('/', createBookingCheckout, isLoggedIn, getOverview);
+router.get('/tour/:slug', isLoggedIn, getTour);
+router.get('/login', isLoggedIn, getLoginForm);
+router.get('/profile', protectFront, getProfile);
+router.get('/my-tours', protectFront, getMyTours);
 module.exports = router;    
