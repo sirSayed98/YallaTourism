e0b845018fe1186f1e700adad26831948c81739f
sirSayed98 <mohamed.sayed8899@gmail.com>
2020-07-13 15:07:26 +0200

    fix bug in create Booking

--- controllers/booking.js
+++ controllers/booking.js
@@ -66,13 +66,12 @@ exports.webhookCheckout = (req, res, next) => {
     res.status(200).json({ received: true });
 };
 
-exports.createBookingCheckout = asyncHandler(async (req, res, next) => {
+const createBookingCheckout = async session => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
     const price = session.display_items[0].amount / 100;
     await Booking.create({ tour, user, price });
-});
-
+};
 
 
 // @desc      Show Booked Tour
